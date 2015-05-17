96baf1f6b9e9951eb3d6e63e2a328d9bde1f0c89
amathewk <amathewk1@gmail.com>
2015-05-17 14:07:31 -0400

    basic validation, counting and stop

--- src/scaloid/example/HelloScaloid.scala
+++ src/scaloid/example/HelloScaloid.scala
@@ -7,11 +7,16 @@ import org.scaloid.common._
 import android.graphics.Color
 
 import scala.collection.mutable
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 class HelloScaloid extends SActivity {
 
   var tts : TextToSpeech = null
 
+  import scaloid.example.CountingState._
+  @volatile var countState : CountingState = Initializing
 
   onCreate {
     contentView = new SVerticalLayout {
@@ -29,16 +34,16 @@ class HelloScaloid extends SActivity {
       this += new SLinearLayout {
         //        STextView("Button: ")
         SButton(R.string.start).id(R.id.count_button)
+        SButton(R.string.stop).id(R.id.stop_button)
       }.wrap
       //      SEditText("Yellow input field fills the space").fill
     } padding 20.dip
 
     tts = new TextToSpeech(getApplicationContext(), ttsInitListener)
-    tts.setSpeechRate(0.75f)
+//    tts.setSpeechRate(1.75f)
 
     find[Button](R.id.count_button).onClick(count())
-
-
+    find[Button](R.id.stop_button).onClick(stopCounting())
 
   }
 
@@ -49,28 +54,37 @@ class HelloScaloid extends SActivity {
 
     val convertResult: ConvertResult = convert(count, time)
     if(convertResult.valid == true) {
-      countdown(convertResult.count, convertResult.time)
+      Future {
+        countdown(convertResult.count.get, convertResult.time.get)
+      }
     } else {
 
       val msg = convertResult.msgs.foldLeft("") { (s1: String, s2: String) => s1 + "\n" + s2 }
       toast(msg)
     }
 
-
-    //    toast(count)
   }
 
+  def stopCounting() = {
 
+      if(countState == Counting) {
+        countState = Interrupting
+      }
+  }
 
+  /** validates the count and integer entries */
   def convert(count: String, time: String) : ConvertResult = {
 
     var valid = true
     val msgs = mutable.HashSet[String]()
 
-    if( ! (count forall Character.isDigit) && (time forall Character.isDigit) ) {
+    if( ! (count forall Character.isDigit) || ! (time forall Character.isDigit) ) {
       valid = false
       msgs += "Count and time must be numbers"
+
+      return ConvertResult(valid, msgs, None, None)
     }
+
 
     val countVal : Int = count.toInt
 
@@ -86,45 +100,64 @@ class HelloScaloid extends SActivity {
       msgs += "time cannot be greater than 1000"
     }
 
-    ConvertResult(valid, msgs, countVal, timeVal)
+    if(valid) {
+      val delay:Float = (timeVal*1000)/countVal
+      if(delay < 50) {
+        valid = false
+        msgs += "The count is too high for the specified time. Either reduce the count or increase the time."
+        return ConvertResult(valid, msgs, None, None)
+      }
+    }
+
+    ConvertResult(valid, msgs, Some(countVal), Some(timeVal))
   }
 
-
-  var ttsStarted = false
 
   def ttsInitListener = new OnInitListener {
 
     override def onInit(i: Int): Unit = {
-      ttsStarted = true
+      countState = Ready
     }
   }
 
 
 
-  def countdown(count: Int, time: Int) = {
+  def countdown(count: Int, time: Int) : Unit = {
 
-    while(!ttsStarted) {}
+    if(countState == Counting) return
+
+    while(countState == Initializing) {}
+
+    countState = Counting
 
     val startTime = System.nanoTime()
 
-    val delay = (time * 1000 )/ count
-    println (s"Delay $delay")
+    val delayInMillis = (time * 1000 )/ count
+    println (s"Delay $delayInMillis")
 
     for(i <- 1 to count){
       tts.speak(i.toString, TextToSpeech.QUEUE_ADD, null)
-      Thread.sleep(delay)
+
+      if(countState == Interrupting) {
+        countState = Ready
+        return
+      }
+
+      Thread.sleep(delayInMillis)
     }
 
     println((System.nanoTime() - startTime))
 
+    countState = Ready
 //    toast(count)
-  }
-
-  def speakCounts() = {
-
   }
 
 }
 
 
-case class ConvertResult(valid: Boolean, msgs: mutable.Set[String], count: Int, time: Int)
+case class ConvertResult(valid: Boolean, msgs: mutable.Set[String], count: Option[Int], time: Option[Int])
+
+object CountingState extends Enumeration {
+  type CountingState = Value
+  val Initializing, Ready, Counting, Interrupting = Value
+}
---
+++ temp.sc
@@ -0,0 +1,6 @@
+val count:Int = 1000
+val time:Int = 1
+val delay:Float = (time*1000)/count
+
+
+
